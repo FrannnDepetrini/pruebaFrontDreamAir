7fe59730477719ec3a093be2c39fbaf5b8100007
Depeex <fdepeex@gmail.com>
2024-09-11 16:00:23 -0300

    Prueba React-Route

---
+++ pruebaFrontDreamAir/src/routes/login.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div>
+      <p>login</p>
+      <Link to="/main">main</Link>
+    </div>
+  );
+};
+
+export default Login;
---
+++ pruebaFrontDreamAir/src/routes/main.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <p>main</p>
+      <Link to="/login">login</Link>
+    </div>
+  );
+};
+export default Main;
